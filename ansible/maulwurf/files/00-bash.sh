479e81cf379e9dbdb51f41c07552b0febbb6c16e
sujiba <simon@buschhaus.koeln>
2026-05-13 21:34:00 +0200

    chore(doco-cd): preparations

---
+++ ansible/maulwurf/files/00-bash.sh
@@ -0,0 +1,15 @@
+## custom bash changes
+# colorful bash
+force_color_prompt=yes
+export LS_OPTIONS='--color=auto'
+eval "`dircolors`"
+# aliases
+alias l='ls $LS_OPTIONS -la'
+alias ll='ls $LS_OPTIONS -l'
+alias vi='vim'
+alias docker-compose='docker compose'
+# search history with keys
+bind '"\e[A": history-search-backward'
+bind '"\e[B": history-search-forward'
+bind '"\e[5~": history-search-backward'
+bind '"\e[6~": history-search-forward'
